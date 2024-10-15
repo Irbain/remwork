86b83b5cc38a286aeab57cf26bc16fe402fbb813
Irbain <contact@irbaine.com>
2024-10-15 15:37:28 +0100

    Done

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,16 @@ import MaxWidthWrapper from "./components/MaxWidthWrapper";
 import TopSection from "./components/sections/TopSection";
 import { AccordionDemo } from "./components/sections/AccordionDemo";
 import Advantages from "./components/sections/Advantages";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <TopSection />
       <Advantages />
+      <MaxWidthWrapper>
+        <AccordionDemo />
+      </MaxWidthWrapper>
       <Divider />
 
       {/* <SettingsLayout>
@@ -18,9 +22,20 @@ export default function Home() {
         
       </SettingsLayout> */}
       <NewsLetters />
-      <MaxWidthWrapper>
-        <AccordionDemo />
-      </MaxWidthWrapper>
+
+      <div className="text-center relative mx-auto max-w-sm">
+        <h3 className="font-semibold text-[#FACC15]">Are You A Recruiter?</h3>
+        <p className="mt-2 text-sm text-gray-300">
+          If you&apos;d like to post your offers, you can do so in minutes.{" "}
+          <Link
+            href="/rectruiter"
+            className="whitespace-nowrap font-medium text-[#FACC15] hover:text-[#FACC15]/80"
+          >
+            Get started &rarr;
+          </Link>
+        </p>
+      </div>
+
       <Footer />
     </div>
   );
